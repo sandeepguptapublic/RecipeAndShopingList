673e139743846be569951f1a4b19bd19ff865966
Sandeep Gupta <sandeepguptapublic@gmail.com>
2023-06-22 17:32:11 -0400

    Passing recipe from recipe-item -> recipe-list -> recipe using EventEmitter. Then using @Input() reading the selected recipe from recipe component to recipe-detail component.

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Recipe } from '../../recipe.model';
 
 @Component({
@@ -8,5 +8,10 @@ import { Recipe } from '../../recipe.model';
 })
 export class RecipeItemComponent {
   @Input() recipe: Recipe;
+  @Output() selectedRecipe = new EventEmitter<Recipe>();
 
+  onRecipeClick(recipeItem: Recipe){
+    this.selectedRecipe.emit(recipeItem);
+
+  }
 }
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,10 +7,18 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
+  @Output() selectedRecipeForRecipeComponent = new EventEmitter<Recipe>();
+
   recipes: Recipe[] = [
     new Recipe('Masala Chai', 'India\'s favourite masala chai', 
     'https://i.pinimg.com/736x/fc/c8/00/fcc80030fa5fd37d93469e06b3380dd7.jpg'),
     new Recipe('traditional-korean-dish-bibimbap', 'This is traditional-korean-dish-bibimbap', 
     'https://i0.wp.com/picjumbo.com/wp-content/uploads/traditional-korean-dish-bibimbap-with-chopsticks-free-photo.jpg?w=2210&quality=70')
   ];
+
+  // locallySelectedRecipe: Recipe;
+
+  onSelectedRecipe(recipe: Recipe){
+    this.selectedRecipeForRecipeComponent.emit(recipe);
+  }
 }
